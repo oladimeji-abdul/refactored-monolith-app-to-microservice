3de1c12ec7b62422f84e563eb0b52164614bd2b2
Abdullahi Oladimeji <abdullahioladimeji90@gmail.com>
2022-09-23 00:27:50 +0100

    final

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a50ff1f5587ff4946bf45c51a6419749-176848455.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
